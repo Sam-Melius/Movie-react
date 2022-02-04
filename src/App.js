2779681afeb88f468f01fc0d792d6dc104ba7441
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-03 18:00:27 -0700

    added movieForm in app.js, rendering

--- src/App.js
+++ src/App.js
@@ -18,7 +18,18 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div>
+        <MovieForm
+          titleForm={titleForm}
+          setTitleForm={setTitleForm}
+          directorForm={directorForm}
+          setDirectorForm={setDirectorForm}
+          yearForm={yearForm}
+          setYearForm={setYearForm}
+          colorForm={colorForm}
+          setColorForm={setColorForm}
+          addMovie={addMovie} />
+      </div>
     </div>
   );
 }
